257b3b7172687bd04d69fdd2fc05ce3aec7f2f9f
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-06-08 23:44:16 +0300

    Alter columns start_date, end_date type to 'datetime with timestamp', modifying  the same columns in models.py

--- service_api/domain/models.py
+++ service_api/domain/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Numeric, Date, MetaData, Table
+from sqlalchemy import Column, String, Numeric, DateTime, MetaData, Table
 from sqlalchemy.dialects.postgresql import UUID
 
 metadata = MetaData()
@@ -8,8 +8,8 @@ contract = Table(
                  Column('id', UUID, primary_key=True),
                  Column('title', String(50), nullable=False),
                  Column('amount', Numeric),
-                 Column('start_date', Date),
-                 Column('end_date', Date),
+                 Column('start_date', DateTime),
+                 Column('end_date', DateTime),
                  Column('customer', String(50), nullable=False),
                  Column('executor', String(50), nullable=False),
                  )
--- service_api/resource/api_v1.py
+++ service_api/resource/api_v1.py
@@ -7,6 +7,4 @@ app.config.RESPONSE_TIMEOUT = 300
 
 app.add_route(SmokeResource.as_view(), '/')                   # GET for Http.status OK
 app.add_route(Contracts.as_view(), '/contracts')              # GET, POST, PUT, DELETE
-app.add_route(Contract.as_view(), '/contract/<contract_id>')  # GET for 1 item
-
-
+app.add_route(Contract.as_view(), '/contract/<contract_id>')  # GET, PUT, DELETE for 1 item
