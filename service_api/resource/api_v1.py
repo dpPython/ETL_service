435508410d346cf95ab9e6850cdbeb1033c3ce73
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-06-16 00:24:37 +0300

    Added docstrings to routes.

--- service_api/resource/api_v1.py
+++ service_api/resource/api_v1.py
@@ -6,8 +6,37 @@ from service_api.resource.views import Contract, Contracts, PaymentsByContracts
 app = Sanic()
 app.config.RESPONSE_TIMEOUT = 300
 
+'''
+Smoke resource with allowed method GET method
+The main purpose is to indicate that the service is working.
+'''
 app.add_route(SmokeResource.as_view(), '/')
+
+'''
+Contracts resource with allowed methods GET, POST, PUT, PATCH, DELETE
+The main purpose is to:
+ - get all contracts in json
+ - get contracts with specified parameters in json
+ - create contracts with specified values and return that instances in json
+ - replace data in specified contracts and return that instances in json
+ - change some data in specified contract and return that instances in json
+ - delete contracts with specified ids and return dict with values of
+        deleted ids and ids that are absence in database if it is true.
+'''
 app.add_route(Contracts.as_view(), '/contracts')
+
+'''
+Contract resource with allowed methods GET, PUT, PATCH, DELETE
+The same functionality as Contracts but only for one instance and except 
+of creating an instance of contract.
+The id of instance must be specified in url.
+'''
 app.add_route(Contract.as_view(), '/contract/<contract_id>')
+
+'''
+PaymentsByContracts resource has only GET method.
+ The main purpose is to get all payments in json with contract ids that are
+ specified in url
+'''
 app.add_route(PaymentsByContracts.as_view(),
               '/payments_by_contracts/<contract_ids>')
